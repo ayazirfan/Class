4fe37af9d1b0b0398acbd4b0cb50a94fc3901c21
ayazirfan <byirfanayaz@gmail.com>
2024-01-24 19:25:11 +0300

    Patika.dev

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿Calisan calisan1 = new Calisan("Ayşe", "Kahraman", 1120, "İnsan Kaynakları");
+System.Console.WriteLine("******Çalışan 1*****");
+calisan1.CalisanBilgileri();
+
 Console.WriteLine("************");
+Calisan calisan2 = new Calisan("Deniz", "Arda", 2324, "Lojistik");
+System.Console.WriteLine("******Çalışan 2*****");
+calisan2.CalisanBilgileri();
+
+Calisan calisan3 = new Calisan("Deniz", "Arda");
+System.Console.WriteLine("******Çalışan 3*****");
+calisan3.CalisanBilgileri();
 
 
 
@@ -21,9 +31,14 @@ class Calisan
    this.No = no;
    this.Departman = departman;
  } 
+ public Calisan(string ad, string soyad)
+ {
+  this.Ad = ad;
+  this.Soyad = soyad;
+ }
  public void CalisanBilgileri()
  {
-  Console.WriteLine("Çalışanın Adı:{0", Ad);
+  Console.WriteLine("Çalışanın Adı:{0}", Ad);
   Console.WriteLine("Çalışanın Soyadı:{0}", Soyad);
   Console.WriteLine("Çalışanın Numarası:{0}", No);
   Console.WriteLine("Çalışanın Departmanı : {0}", Departman);
